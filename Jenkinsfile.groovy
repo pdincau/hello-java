5d3a69f24a3ca624bb0abfdb052ebd9407126085
Paolo D'Incau <paolo.dincau@gmail.com>
2017-11-06 14:52:39 +0100

    update jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -18,11 +18,13 @@ node {
 
     stage('UAT') {
         docker.image('pdincau/hello-java').withRun('-p 9999:8080') { c ->
+            sh 'sleep 5'
             sh 'curl -v 127.0.0.1:9999/ping'
         }
     }
 
     stage('Publish Docker Image') {
+        sh "docker push pdincau/hello-java"
         docker.withRegistry('https://registry.hub.docker.com', 'pdincau-credentials') {
             app.push("${env.BUILD_NUMBER}")
             app.push("latest")
